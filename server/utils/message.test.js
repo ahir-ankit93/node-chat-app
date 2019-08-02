10bb4d03a7afd859d002366aee73d797edc47039
ahir-ankit93 <ahir.ankit93@gmail.com>
2019-08-02 14:59:45 +0530

    create generateMessage utility

---
+++ server/utils/message.test.js
@@ -0,0 +1,32 @@
+// var expect = require('expect');
+
+// var {generateMessage} = require('./message');
+
+// describe('generateMessage', () => {
+//     it('should generate correct message object', () => {
+//         var from = 'jen';
+//         var text = 'Some message';
+//         var message = generateMessage(from, text);
+
+//         expect(message.createdAt).toBeA('number');
+//         expect(message).toInclude({from, text});
+
+//     });
+// });
+
+
+var expect = require('expect');
+var {generateMessage}  = require('./message');
+
+describe('generateMessage', () => {
+   it('should generate correct message object', () => {
+
+       var from = 'Jen';
+       var text = 'Some message';
+       var message = generateMessage(from, text);
+
+       expect(typeof message.createdAt).toBe('number');
+       expect(message).toMatchObject({from, text});
+   });
+
+});
