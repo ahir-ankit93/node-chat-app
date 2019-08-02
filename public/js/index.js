9689a80906cbee51c7675f6e4a24fe4ded0a15a0
ahir-ankit93 <ahir.ankit93@gmail.com>
2019-08-02 16:13:01 +0530

    Add from for message and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -15,4 +15,26 @@ var socket = io();
 
         socket.on('newMessage', function (message) {
             console.log('newMessage', message);
+            var li = jQuery('<li></li>');
+            li.text(`${message.from}: ${message.text}`);
+
+            jQuery('#messages').append(li);
+        });
+
+        // socket.emit('createMessage', {
+        //     from: 'Frank',
+        //     text: 'Hi'
+        // }, function (data) {
+        //     console.log('Got it', data);
+        // });
+
+        jQuery('#message-form').on('submit', function (e) {
+            e.preventDefault();
+
+            socket.emit('createMessage', {
+                from: 'User',
+                text: jQuery('[name=message]').val()
+            }, function(){
+
+            });
         });
