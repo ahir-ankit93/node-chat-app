a5103d6c39bf296afd7bc3e285a17885670e4413
lcomu19.4 <lcomu194@lcomu194.4>
2019-08-02 10:28:02 +0530

    Init commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port, ()=>{
+    console.log(`Server is up on ${port}`);
+});
+
+
+
+// console.log(__dirname + '/../public');
+// console.log();
