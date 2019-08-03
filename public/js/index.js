c091a38f8d43877f8390d12c05a3cdf0d942bd86
ahir-ankit93 <ahir.ankit93@gmail.com>
2019-08-03 14:37:15 +0530

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,21 @@
 var socket = io();
 
-    // function scrollToBottom () {
+    function scrollToBottom () {
         // Selectors
-        // var message = jQuery('#messages');
+        var messages = jQuery('#messages');
+        var newMessage = messages.children('li:last-child')
  
         // Heights
-        // var clientHeight = message.prop('clientHeight');
-        // var scrollTop = message.prop()
-    // }
+        var clientHeight = messages.prop('clientHeight');
+        var scrollTop = messages.prop('scrollTop');
+        var scrollHeight = messages.prop('scrollHeight');
+        var newMessageHeight =newMessage.innerHeight();
+        var lastMessageHeight = newMessage.prev().innerHeight();
+
+        if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+            console.log('should scroll');
+        }
+    }
 
         socket.on('connect', () => {
             console.log('Connected to server');
@@ -33,7 +41,7 @@ var socket = io();
             });
 
             jQuery('#messages').append(html);
-            // scrollToBottom();
+            scrollToBottom();
        
             // console.log('newMessage', message);
         //    var formattedTime = moment(message.createdAt).format('h:mm a');
@@ -54,7 +62,7 @@ var socket = io();
             });
 
             jQuery('#messages').append(html);
-            // scrollToBottom();
+            scrollToBottom();
 
             // var formattedTime = moment(message.createdAt).format('h:mm a');
             // var li = jQuery('<li></li>');
